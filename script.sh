da938aa81497ec87cd1ed84729c3d93208369656
Fabiano Vandré <69203660+Fabiano-Vandre@users.noreply.github.com>
2025-02-26 15:50:29 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo dnf check-update
-sudo dnf upgrade -y
-sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo yum check-update
+sudo yum upgrade -y
+sudo yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
