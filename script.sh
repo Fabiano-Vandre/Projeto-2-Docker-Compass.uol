5d906fabbafc0f040f7ec29024bbd203e38652eb
Fabiano Vandré <69203660+Fabiano-Vandre@users.noreply.github.com>
2025-02-26 15:47:02 -0300

    Criação do Script

---
+++ script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo dnf check-update
+sudo dnf upgrade -y
+sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
